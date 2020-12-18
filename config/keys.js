7ed8a56715ccd3a20b6d1084a97bb7648ad090e5
Ritika Lath <lathritika2@gmail.com>
2020-12-18 16:31:38 +0530

    login and signup

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+    mongoURI: "mongodb+srv://admin123:admin123@electoral-roll.22hvm.mongodb.net/electrol-roll-database?retryWrites=true&w=majority" ,
+    secretOrKey: "secret"
+  };
